601807d4bf611a888c4a777fadb5135f43a201fa
Martin Heistermann <git@mheistermann.de>
2026-02-10 13:35:44 +0100

    IPC with country flags

--- scripts/ipc.py
+++ scripts/ipc.py
@@ -1,6 +1,9 @@
 import yaml
 import csv
+from collections import namedtuple
 
+
+Person = namedtuple("Person", "last first affil country")
 
 class Quoted(str):
     pass
@@ -16,9 +19,12 @@ def quoted_representer(dumper, data):
 
 yaml.SafeDumper.add_representer(Quoted, quoted_representer)
 
-rows = list(csv.reader(open("ipc.csv")))[1:]
+rows = list(Person(*x) for x in csv.reader(open("ipc.csv")))[1:]
 
-data = [dict(name = Quoted(f"{first} {last}"), affiliation=Quoted(aff)) for (last,first,aff) in rows]
+data = [dict(name = Quoted(f"{p.first} {p.last}"),
+             affiliation=Quoted(p.affil),
+             country=Quoted(p.country),
+             ) for p in rows]
 print(yaml.dump(
     data,
     Dumper=yaml.SafeDumper,

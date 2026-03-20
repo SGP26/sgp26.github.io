396eea74714fcb20347d5ad5f90e5c742cc48ab5
Martin Heistermann <git@mheistermann.de>
2026-03-20 22:12:08 +0100

    Add deadline partial to display dates that get formatted differently when expired.

--- themes/minimalConference/assets/js/deadline-date.js
+++ themes/minimalConference/assets/js/deadline-date.js
@@ -8,11 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
   deadlineSpans.forEach(function (span) {
     const dateString = span.getAttribute("expires-on");
     const deadlineDate = new Date(dateString);
+    if (!isNaN(deadlineDate)) {
       if (currentDate > deadlineDate) {
           console.log("expired");
         span.classList.add("deadline-expired");
       } else {
-          console.log("time left: ", (deadlineDate-currentDate)/1000/3600/24, " days);
+          console.log("time left: ", (deadlineDate-currentDate)/1000/3600/24, " days");
       }
     } else {
       console.warn(`Invalid date format in deadline span: ${dateString}`);

40b6959dbad779479e2cab06416240d6d3f0c8ba
Martin Heistermann <git@mheistermann.de>
2026-03-20 22:10:31 +0100

    deadline styling

---
+++ themes/minimalConference/assets/js/deadline-date.js
@@ -0,0 +1,21 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const deadlineSpans = document.querySelectorAll(
+    "span[expires-on]",
+  );
+  const currentDate = new Date();
+  deadlineSpans.forEach(function (span) {
+    const dateString = span.getAttribute("expires-on");
+    const deadlineDate = new Date(dateString);
+      if (currentDate > deadlineDate) {
+          console.log("expired");
+        span.classList.add("deadline-expired");
+      } else {
+          console.log("time left: ", (deadlineDate-currentDate)/1000/3600/24, " days);
+      }
+    } else {
+      console.warn(`Invalid date format in deadline span: ${dateString}`);
+    }
+  });
+});
